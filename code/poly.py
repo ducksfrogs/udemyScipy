32de0f52ea46800626741b2cb0338fa417a5ac0d
ma <ma_yamaki@yahoo.com>
2020-11-12 10:01:45 +0900

    tukareta

--- code/poly.py
+++ code/poly.py
@@ -4,3 +4,28 @@ import matplotlib.pyplot as plt
 
 
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import ShuffleSplit
+from sklearn.datasets import load_breast_cancer
+
+data = load_breast_cancer()
+
+
+ss = ShuffleSplit(n_splits=1,
+                  train_size=0.8,test_size=0.2)
+
+
+X = data.data
+y = data.target
+
+train_index, test_index = next(ss.split(X, y))
+
+X_train, X_test = X[train_index], X[test_index]
+y_train,y_test = y[train_index], y[test_index]
+
+
+polf = PolynomialFeatures(degree=2)
+
+polf.fit(X_train)
+
+X_train_poly = polf.transform(X_train)
+X_test_poly =polf.transform(X_test)
