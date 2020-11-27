7f1aac3d8faff20a22a97f40e8cdbbd9378b400a
ma <ma_yamaki@yahoo.com>
2020-11-27 16:51:52 +0900

    dd

--- code/gridSearch.py
+++ code/gridSearch.py
@@ -23,3 +23,23 @@ clf = LogisticRegression(solver='liblinear')
 
 
 C_range = [1e-5, 1e-3, 1e-2, 1, 1e2, 1e5, 1e10]
+
+C_range_exp = np.arange(-15, 21)
+C_range = 10 ** C_range_exp
+
+import warnings
+
+from sklearn.exceptions import ConvergenceWarning
+warnings.simplefilter('ignore',ConvergenceWarning)
+
+from sklearn.model_selection import GridSearchCV
+
+param = {'C':C_range}
+
+gs = GridSearchCV(clf, param, return_train_score=True)
+gs.fit(X_train, y_train)
+
+clf_best = gs.best_estimator_
+clf_best.score(X_test, y_test)
+
+gs.score(X_test, y_test)
