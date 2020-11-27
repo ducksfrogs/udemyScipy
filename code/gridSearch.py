4481eaa7b93dc980a042f778692980bf73664418
ma <ma_yamaki@yahoo.com>
2020-11-27 17:19:45 +0900

    dd

--- code/gridSearch.py
+++ code/gridSearch.py
@@ -43,3 +43,61 @@ clf_best = gs.best_estimator_
 clf_best.score(X_test, y_test)
 
 gs.score(X_test, y_test)
+
+
+#plot error bar
+
+plt.errorbar(gs.cv_results_['param_C'].data,
+            gs.cv_results_['mean_strain_score'],
+            yerr=gs.cv_results_['std_train_score'],
+            label="training")
+
+plt.errorbar(gs.cv_results_['param_C'].data,
+            gs.cv_results_['mean_test_score'],
+            yerr=gs.cv_results_['std_test_score'], label="test(val)")
+
+plt.ylim(.6, 1.01)
+plt.xscale("log")
+plt.xlabel("C")
+plt.ylabel("accuracy")
+plt.legend(loc="best")
+
+
+plt.errorbar(gs.cv_results_['param_C'].data,
+            gs.cv_results_['mean_score_time'],
+            yerr=gs.cv_results_['std_score_time'],
+            label="test(val)")
+
+
+plt.ylim(0,)
+plt.xscale("log")
+plt.xlabel("C")
+plt/plt.ylabel("computation time")
+plt.legend(loc="best")
+
+
+
+
+#SVC
+
+from sklearn.svm import SVC
+
+clf = SVC(gamma='auto')
+
+C_range_exp = np.arange(-2.0, 5.0)
+C_range = 10.0 ** C_range_exp
+
+param = {'C': C_range,
+        'kernel': ['linear', 'rbf']}
+
+gs = GridSearchCV(clf, param, n_jobs=-1, verbose=2, return_train_score=True)
+
+gs.best_params_,
+gs.best_score_,
+gs.best_estimator_
+
+s_linear = (gs.cv_results_['param_kernel']=='linear').data
+
+plt.plot(gs.cv_results_['param_C'][s_linear].data,
+        gs.cv_results_['mean_strain_score'][s_linear],
+        label='training (linear)')
