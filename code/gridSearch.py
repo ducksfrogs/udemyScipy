040e8b96aa3135892e503f119037ec415fe57e36
ma <ma_yamaki@yahoo.com>
2020-11-27 11:26:06 +0900

    dd

---
+++ code/gridSearch.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
+
+from sklearn.model_selection import train_test_split
+
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=8, test_size=0.2)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
