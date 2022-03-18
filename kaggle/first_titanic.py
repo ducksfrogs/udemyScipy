b473082c250e69c5a00c684937bde914b6fc5f8f
ducksfrogs <ma_yamaki@yahoo.com>
2022-03-18 21:30:53 +0900

    dd

--- kaggle/first_titanic.py
+++ kaggle/first_titanic.py
@@ -15,26 +15,37 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 
-train_df = pd.read_csv('../input/train.csv')
-test_df = pd.read_csv('../input/test.csv')
+train_df = pd.read_csv("../input/train.csv")
+test_df = pd.read_csv("../input/test.csv")
 combine = [train_df, test_df]
 
 print(train_df.columns.values)
 
 
 train_df.info()
-print('_'*20)
+print("_" * 20)
 test_df.info()
 
 train_df.describe()
 
-train_df.describe(include=['O'])
+train_df.describe(include=["O"])
 
-train_df[['Pclass', 'Survived']].groupby(
-    ['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["Pclass", "Survived"]].groupby(["Pclass"], as_index=False).mean().sort_values(
+    by="Survived", ascending=False
+)
 
-train_df[["Sex", "Survived"]].groupby(
-    ['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["Sex", "Survived"]].groupby(["Sex"], as_index=False).mean().sort_values(
+    by="Survived", ascending=False
+)
 
-train_df[["SibSp", "Survived"]].groupby(
-    ['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["SibSp", "Survived"]].groupby(["SibSp"], as_index=False).mean().sort_values(
+    by="Survived", ascending=False
+)
+
+train_df[["Parch", "Survived"]].groupby("Parch", as_index=False).mean().sort_values(
+    by="Survived", ascending=False
+)
+
+
+g = sns.FacetGrid(train_df, col="survived")
+g.map(plt.hist, "Age")
